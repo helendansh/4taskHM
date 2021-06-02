52bde8724b4545ef06f95029f4c431d0b33817db
helendansh <danshinahel@yandex.ru>
2021-06-02 15:41:40 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Map<String,Integer> testmap = createMap();
+        removeItemFromMap(testmap);
+    //  System.out.println(testmap);  //проверка
+    }
+
+    public static Map<String, Integer> createMap () {
+        Map <String, Integer> map = new HashMap<>();
+        map.put("Иванов", 379);
+        map.put("Петров", 945);
+        map.put("Смирнов", 1678);
+        map.put("Воробьева", 1794);
+        map.put("Крылова", 368);
+        map.put("Быков", 580);
+        map.put("Дятлов", 1367);
+        map.put("Волкова", 367);
+        map.put("Зайцев", 963);
+        map.put("Семенова", 457);
+        return map;
+    }
+
+    public static void removeItemFromMap(Map <String, Integer> map) {
+        map.entrySet().removeIf(i -> i.getValue() < 500);
+    }
+}
